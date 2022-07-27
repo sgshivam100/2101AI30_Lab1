7012230c2b51eaa958d7b5e61caaa9df6c916cad
sgshivam100 <sg100shivam@gmail.com>
2022-07-27 22:33:25 +0530

    main and exit

--- sorting.c
+++ sorting.c
@@ -131,3 +131,118 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+
+
+int main(){
+    printf("Choose one options from the following:\n");
+    printf("1. Insertion Sort\n");
+    printf("2. Selection Sort\n");
+    printf("3. Bubble Sort\n");
+    printf("4. Merge Sort\n");
+    printf("5. Quick Sort\n");
+    printf("6. Exit\n");
+    int choice;
+    scanf("%d", &choice);
+    if(choice == 1){
+        printf("Insertion Sort\n");
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        insertionSort(arr, n);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else if(choice == 2){
+         printf("Selection Sort\n");
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        selectionSort(arr, n);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else if(choice == 3){
+        printf("Bubble Sort\n");
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        bubbleSort(arr, n);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else if(choice == 4){
+        printf("Merge Sort\n");
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        mergeSort(arr, 0, n - 1);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else if(choice == 5){
+        printf("Quick Sort\n");
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        quickSort(arr, 0, n - 1);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else if(choice == 6){
+        printf("Exiting...\n");
+    }
+    else{
+        printf("Invalid choice. Exiting...\n");
+    }
+    return 0;
+}
